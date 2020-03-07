21070ac40678cb68cbc7f8e356d2c8f52874bb79
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2020-03-07 01:30:02 -0300

    Update mergecsv.sh

--- mergecsv.sh
+++ mergecsv.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/bin/bash -e
 shopt -s nullglob
 
 arr=(part-*.csv)
 
 first=${arr[0]}
-
 final=$1.csv
 
-head -1 $first > $final
+for filename in "${arr[@]}";
+do
+  if [ -s $filename ];
+  then
+    first=$filename
+  fi
+done
 
-for filename in $arr; do sed 1d $filename >> $final; done
+echo $first
+
+head -1 $filename > $final
+
+for filename in "${arr[@]}"; do sed 1d $filename >> $final; done
